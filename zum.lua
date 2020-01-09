3adf1ec23e51bea38ba01db59a5951aa7a73247a
Ray Depew <rdepew@freewave.com>
2020-01-09 10:10:23 -0700

    Add labels and fields to ZumLink viewer
    Code copied from Corona Labs' business-app-sample, at
    https://github.com/coronalabs-samples/business-app-sample.git

--- zum.lua
+++ zum.lua
@@ -4,6 +4,23 @@ local widget = require( "widget" ) -- for the pushbutton
 
 local scene = composer.newScene()
 
+local ipAddrField
+local snField
+local devNameField
+local modelField
+local fwField
+local rteField
+local licenseField
+local uptimeField
+local ipAddrLabel
+local snLabel
+local devNameLabel
+local modelLabel
+local fwLabel
+local rteLabel
+local licenseLabel
+local uptimeLabel
+
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
@@ -13,6 +30,26 @@ local function gotoMenu()
   composer.gotoScene( "menu" )
 end
 
+local function fieldHandler( textField )
+	return function( event )
+		if ( "began" == event.phase ) then
+			-- This is the "keyboard has appeared" event
+			-- In some cases you may want to adjust the interface when the keyboard appears.
+		
+		elseif ( "ended" == event.phase ) then
+			-- This event is called when the user stops editing a field: for example, when they touch a different field
+			
+		elseif ( "editing" == event.phase ) then
+		
+		elseif ( "submitted" == event.phase ) then
+			-- This event occurs when the user presses the "return" key (if available) on the onscreen keyboard
+			print( textField().text )
+			
+			-- Hide keyboard
+			native.setKeyboardFocus( nil )
+		end
+	end
+end
 
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
@@ -106,6 +143,53 @@ function scene:show( event )
 
 	elseif ( phase == "did" ) then
 		-- Code here runs when the scene is entirely on screen
+                --
+		-- The text field's native piece starts hidden, we show it after we are on screen.on
+
+		-- lets make the fields fit our adaptive screen better
+		-- Why 150? The labels are around 120px wide. We want at least a 10px margin on either side of the labels
+		-- and fields and we need some space betwen the label and the field. Let's start with 10px each
+
+		local fieldWidth = display.contentWidth - 150
+		if fieldWidth > 250 then
+			fieldWidth = 250
+		end
+
+		ipAddrField = native.newTextField( 130, ipAddrLabel.y, fieldWidth, 30 )
+		ipAddrField:addEventListener( "userInput", fieldHandler( function() return ipAddrField end ) ) 
+		sceneGroup:insert( ipAddrField)
+		ipAddrField.anchorX = 0
+		ipAddrField.placeholder = "xxx.xxx.xxx.xxx"
+
+		snField = native.newTextField( 130, snLabel.y, fieldWidth, 30 )
+		sceneGroup:insert( snField)
+		snField.anchorX = 0
+
+		devNameField = native.newTextField( 130, devNameLabel.y, fieldWidth, 30 )
+		sceneGroup:insert( devNameField)
+		devNameField.anchorX = 0
+
+		modelField = native.newTextField( 130, modelLabel.y, fieldWidth, 30 )
+		sceneGroup:insert( modelField)
+		modelField.anchorX = 0
+
+		fwField = native.newTextField( 130, fwLabel.y, fieldWidth, 30 )
+		sceneGroup:insert( fwField)
+		fwField.anchorX = 0
+
+		rteField = native.newTextField( 130, rteLabel.y, fieldWidth, 30 )
+		sceneGroup:insert( rteField)
+		rteField.anchorX = 0
+
+		licenseField = native.newTextField( 130, licenseLabel.y, fieldWidth, 30 )
+		sceneGroup:insert( licenseField)
+		licenseField.anchorX = 0
+
+		uptimeField = native.newTextField( 130, uptimeLabel.y, fieldWidth, 30 )
+		sceneGroup:insert( uptimeField)
+		uptimeField.anchorX = 0
+
+                -- TODO: populate the textFields, if they already have values assigned.
 
 	end
 end
@@ -119,6 +203,26 @@ function scene:hide( event )
 
 	if ( phase == "will" ) then
 		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+                -- TODO: Save all textField values before removing them.
+                --
+		-- remove native.newTextFields since they contain native objects.
+		ipAddrField:removeSelf()
+		ipAddrField = nil
+		snField:removeSelf()
+		snField = nil
+		devNameField:removeSelf()
+		devNameField = nil
+		modelField:removeSelf()
+		modelField = nil
+		fwField:removeSelf()
+		fwField = nil
+		rteField:removeSelf()
+		rteField = nil
+		licenseField:removeSelf()
+		licenseField = nil
+		uptimeField:removeSelf()
+		uptimeField = nil
 
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
